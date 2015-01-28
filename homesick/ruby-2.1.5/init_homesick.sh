db4a5bd677aee4777333be283f4cbfb2682ababa
Arai Hiroki <hiroara62@gmail.com>
2015-01-28 13:54:38 +0900

    add Dockerfile of homesick image

---
+++ homesick/ruby-2.1.5/init_homesick.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+HOMESICK_NAME=$(basename $HOMESICK_REPO .git)
+
+if [ ! -d $HOME/.ssh ]; then
+  mkdir $HOME/.ssh
+  cp /ssh/* $HOME/.ssh/
+  chmod 600 $HOME/.ssh/*
+  eval $(ssh-agent)
+  ssh-add
+fi
+
+if [ $HOMESICK_REPO ] && [ ! -d $HOME/.homesick/repos/$HOMESICK_NAME ]; then
+  gem install homesick
+  homesick clone $HOMESICK_REPO
+  homesick symlink $HOMESICK_NAME
+fi
+
+exec "$@"
