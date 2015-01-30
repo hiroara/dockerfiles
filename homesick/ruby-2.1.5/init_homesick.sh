6e6db54ae99d549ebc9a7f564cab2637aa4ea4df
Arai Hiroki <hiroara62@gmail.com>
2015-01-30 11:20:54 +0900

    move gem install to RUN section and detect ssh keys

--- homesick/ruby-2.1.5/init_homesick.sh
+++ homesick/ruby-2.1.5/init_homesick.sh
@@ -9,11 +9,10 @@ if [ -d /ssh ] && [ ! -d $HOME/.ssh ]; then
   cp /ssh/* $HOME/.ssh/
   chmod 600 $HOME/.ssh/*
   eval $(ssh-agent)
-  ssh-add
+  grep -r "BEGIN [^\s]* PRIVATE KEY" ssh | tr : \\t | awk '{ print $1 }' | xargs ssh-add
 fi
 
 if [ $HOMESICK_REPO ] && [ ! -d $HOME/.homesick/repos/$HOMESICK_NAME ]; then
-  gem install homesick
   homesick clone $HOMESICK_REPO
   homesick symlink $HOMESICK_NAME
 fi
